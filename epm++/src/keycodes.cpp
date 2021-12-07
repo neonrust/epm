6f6a7ed1693eaa5ef2fbcd8ae2c99708e5433df4
André Jonsson <andre.jonsson@dataductus.se>
2021-12-07 14:32:53 +0100

    key_from_string() now rejects empty string

--- epm++/src/keycodes.cpp
+++ epm++/src/keycodes.cpp
@@ -63,8 +63,12 @@ std::string to_string(Key k, Modifier m)
 
 Key key_from_string(const std::string &name)
 {
+	if(name.empty())
+		return None;
+
 	if(name.size() == 1 and name[0] >= 'A' and name[0] <= 'Z')
 		return Key(name[0] - 'A' + A);
+
 	if(name[0] == 'F')
 	{
 		if(name.size() == 2) // F1 .. F9
@@ -72,21 +76,22 @@ Key key_from_string(const std::string &name)
 		if(name.size() == 3) // F10 .. F12
 			return Key(name[2] - '0' + F10);
 	}
-	if(name == "BACKSPACE")  return  BACKSPACE;
-	if(name == "TAB")        return  TAB;
-	if(name == "ENTER")      return  ENTER;
-	if(name == "UP")         return  UP;
-	if(name == "DOWN")       return  DOWN;
-	if(name == "RIGHT")      return  RIGHT;
-	if(name == "LEFT")       return  LEFT;
-	if(name == "HOME")       return  HOME;
-	if(name == "INSERT")     return  INSERT;
-	if(name == "DELETE")     return  DELETE;
-	if(name == "END")        return  END;
-	if(name == "PAGE_UP")    return  PAGE_UP;
-	if(name == "PAGE_DOWN")  return  PAGE_DOWN;
-	if(name == "ESCAPE")     return  ESCAPE;
-	if(name == "NUMPAD_5")   return  NUMPAD_5;
+
+	if(name == "BACKSPACE")  return BACKSPACE;
+	if(name == "TAB")        return TAB;
+	if(name == "ENTER")      return ENTER;
+	if(name == "UP")         return UP;
+	if(name == "DOWN")       return DOWN;
+	if(name == "RIGHT")      return RIGHT;
+	if(name == "LEFT")       return LEFT;
+	if(name == "HOME")       return HOME;
+	if(name == "INSERT")     return INSERT;
+	if(name == "DELETE")     return DELETE;
+	if(name == "END")        return END;
+	if(name == "PAGE_UP")    return PAGE_UP;
+	if(name == "PAGE_DOWN")  return PAGE_DOWN;
+	if(name == "ESCAPE")     return ESCAPE;
+	if(name == "NUMPAD_5")   return NUMPAD_5;
 
 	fmt::print(g_log, "unknown key: '{}'\n", name);
 	assert(false);
