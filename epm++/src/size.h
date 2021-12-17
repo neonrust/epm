0b9ed480f8d214d56dcf4f1abe8c8d1b44fb4ca1
André Jonsson <andre.jonsson@dataductus.se>
2021-12-17 14:21:28 +0100

    operator == should be const :/

--- epm++/src/size.h
+++ epm++/src/size.h
@@ -7,7 +7,7 @@ struct Size
 	std::size_t width;
 	std::size_t height;
 
-	inline bool operator == (const Size &t)
+	inline bool operator == (const Size &t) const
 	{
 		return t.width == width and t.height == height;
 	}
