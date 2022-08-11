52f600beafcc923421277bdb094d798f6e41a2ac
André Jonsson <andre.jonsson@dataductus.se>
2022-08-11 17:36:34 +0200

    Divided internal storage into three 'stores'
    - defaults   Used if not set anywhere else
    - persistent Loaded/saved to file
    - memory     Used internally, never persisted.
    
    Renamed main functions to just get/set etc, since they're intended to
    be used in conjunction with the module name,
    e.g. `config.get(...)`. This affects other code, but this commit is
    only the config module.

--- episode_manager/config.py
+++ episode_manager/config.py
@@ -1,17 +1,17 @@
 import os
 from os.path import basename, dirname, expandvars, expanduser, exists as pexists, getsize as psize, join as pjoin
+from typing import Any
 
 from .utils import read_json, write_json, warning_prefix, pexpand
 
-from typing import Any
 
 default_max_refresh_age = 2  # days
 default_max_hits = 10
 
 user_config_home = os.getenv('XDG_CONFIG_HOME') or pexpand(pjoin('$HOME', '.config'))
-app_config_file = ''
 
-PRG = ''
+app_config_file = ''  # set in init()
+PRG = '' # set in init()
 
 def init(prg):
 	global PRG
@@ -20,12 +20,14 @@ def init(prg):
 	app_config_file = pjoin(user_config_home, PRG, 'config')
 
 
-default_configuration = {
+ValueType = str|int|float|list[Any]|dict[str, Any]|None  # "Any" b/c mypy doesn't support recursive type hints
+
+_configuration_defaults:dict[str, ValueType] = {
 	'paths': {
-		'series-db': None,  # defaulted in load_config()
+		#'series-db': N  # defaulted in load()
 	},
 	'commands': {
-		'default': 'unseen',
+		#'default': 'unseen',
 		'calendar': {
 			'num_weeks': 1,
 		},
@@ -33,53 +35,63 @@ default_configuration = {
 	'max-age': default_max_refresh_age,
 	'num-backups': 10,
 	'lookup': {
-		'api-key': None,
+		#'api-key': None,
 		'max-hits': default_max_hits,
 	},
 	'debug': 0,
 }
 
-app_config = {}
-app_config_dirty = False
+_app_config:dict[str, ValueType] = {}
+_app_config_dirty = False  # if True, it needs to be saved to disk
+_memory_config:dict[str, ValueType] = {}  # only used at runtime, not persisted
+
+STORE_PERSISTENT = 'persistent'
+STORE_MEMORY     = 'memory'
+STORE_DEFAULTS   = 'defaults'
+
+_config_stores = {
+	None: _app_config,
+	STORE_PERSISTENT: _app_config,
+	STORE_MEMORY:     _memory_config,
+	STORE_DEFAULTS:   _configuration_defaults,
+}
 
 
 def load() -> bool:
-	global app_config
-	global app_config_dirty
+	_app_config.clear()
 
-	app_config = read_json(app_config_file)
-	app_config_dirty = False
-
-	config = {**default_configuration}
-	config.update(app_config)
-	app_config = config
+	config = read_json(app_config_file)
+	if config:
+		_app_config.update(config)
 
 	db_file = get('paths/series-db')
 	if not db_file or not isinstance(db_file, str):
-		set('paths/series-db', pjoin(user_config_home, PRG, 'series'), dirty=False)
+		db_file = pjoin(user_config_home, PRG, 'series')
+		set('paths/series-db', db_file, store=STORE_MEMORY)
 
-	paths = app_config.get('paths', {})
+	paths = _app_config.get('paths', {})
 	if not isinstance(paths, dict):
 		raise RuntimeError(f'{warning_prefix()} Config key "paths" is not an object')
 
 	for key in paths.keys():
 		paths[key] = pexpand(paths[key])
 
-	app_config_dirty = False
+	global _app_config_dirty
+	_app_config_dirty = False
 
-	return len(app_config) > 0
+	return len(_app_config) > 0
 
 
 def save():
-	global app_config_dirty
-	if not app_config_dirty:
+	global _app_config_dirty
+	if not _app_config_dirty or not _app_config:
 		return
 
-	err = write_json(app_config_file, app_config)
+	err = write_json(app_config_file, _app_config)
 	if err is not None:
 		print(f'{_E}ERROR{_00} Failed saving configuration: %s' % str(err))
 
-	app_config_dirty = False
+	_app_config_dirty = False
 
 # type alias for type hints (should be recursive, but mypy doesn't support it)
 ConfigValue = str|int|float|dict|list
@@ -87,8 +99,12 @@ ConfigValue = str|int|float|dict|list
 def get(path:str, default_value:ConfigValue|None=None, convert=None) -> ConfigValue|None:
 	# path: key/key/key
 	keys = path.split('/')
+	if not keys:
+		raise RuntimeError('Empty key path')
 
-	scope:dict|list|str|int|float|None = app_config
+	config:dict|list|str|int|float|None = _configuration_defaults | _app_config | _memory_config
+	scope = config
+
 	current:list[str] = []
 	for key in keys:
 		# print('cfg: %s + %s' % ('/'.join(current), key))
@@ -119,20 +135,27 @@ def get_int(path:str, default_value:int=0) -> int:
 
 def get_bool(path:str, default_value:bool=False) -> bool:
 	v = get(path, default_value)
-	if isinstance(v, (str, bool)):
+	if isinstance(v, (str, int, bool)):
 		return bool(v)
 	return default_value
 
 
-def set(path:str, value:Any, dirty:bool=True) -> None:
+Store = str
+
+def set(path:str, value:Any, store:Store=STORE_PERSISTENT) -> None:
 	# path: key/key/key
 	keys = path.split('/')
+	if not keys:
+		raise RuntimeError('Empty key path')
 
-	ValueType = dict[str, Any]|list|str|int|float|None  # mypy doesn't support recursive type hints
-
-	scope:ValueType = app_config
-	if not isinstance(scope, dict): # to shut mypy up
+	store = store or STORE_MEMORY
+	config:ValueType = _config_stores.get(store)
+	if store and config is None:
+		raise RuntimeError('invalid config store "%s" (one of %s)' % (store, ', '.join(_config_stores.keys())))
+	if not isinstance(config, dict): # to shut mypy up
 		return None
+
+	scope = config
 
 	current:list[str] = []
 	while keys:
@@ -142,9 +165,10 @@ def set(path:str, value:Any, dirty:bool=True) -> None:
 			scope[key] = value
 			break
 
-		new_scope = scope.get(key)
-		if new_scope is None:  # missing key object
-			scope[key] = {}
+		new_scope:dict = scope.get(key)
+		if new_scope is None:  # missing key (object container)
+			new_scope = {}
+			scope[key] = new_scope
 
 		if not isinstance(new_scope, dict): # exists, but is not an object
 			raise RuntimeError('Invalid path "%s"; not object at "%s", got %s (%s)' % (path, '/'.join(current), scope, type(new_scope).__name__))
@@ -152,6 +176,7 @@ def set(path:str, value:Any, dirty:bool=True) -> None:
 		scope = new_scope
 		current.append(key)
 
-	if dirty:
-		global app_config_dirty
-		app_config_dirty = True
+
+	if store == STORE_PERSISTENT:
+		global _app_config_dirty
+		_app_config_dirty = True
