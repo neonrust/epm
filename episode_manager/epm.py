304c31a5f444ae5d4e86823be1f7daa14d07aa72
André Jonsson <andre.jonsson@dataductus.se>
2023-04-28 14:08:28 +0200

    Added specials support to the `show -I` display

--- episode_manager/epm.py
+++ episode_manager/epm.py
@@ -2216,9 +2216,13 @@ def print_series_details(index:int, series:dict, width:int, gray:bool=False) -> 
 		print(f'    {_o}Guests:   {_0}', f'{_o},{_0} '.join(guests))
 
 	if episodes:
-		num_seasons = max(ep.get('season',0) for ep in episodes)
+		num_specials = len(list(filter(lambda ep: ep.get('season') == 'S', episodes)))
+		num_seasons = max(ep.get('season', 0) if type(ep.get('season')) is int else 0 for ep in episodes)
 		print(f'    {_o}Seasons: {_0}', num_seasons, end='')
-		print(f'  {_f}/{_0}  {_o}Episodes: {_0}', len(episodes))
+		print(f'  {_f}/{_0}  {_o}Episodes: {_0}', len(episodes) - num_specials, end='')
+		if num_specials:
+			print(f'  {_f}/{_0}  {_o}Specials: {_0}', num_specials, end='')
+		print()
 		for season in range(1, num_seasons + 1):
 			print(f'     {_c}{"s%d" % season:>3}{_0}', '%3d' % len(list(filter(lambda ep: ep.get('season') == season, episodes))), 'episodes')
 	else:
